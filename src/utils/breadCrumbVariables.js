38c1d5e524a3d338a54022c1cb278d8b008f4a3c
indra <indramanivelan123@gmail.com>
2019-04-15 23:03:21 +0800

    "Ui changed"

--- src/utils/breadCrumbVariables.js
+++
@@ -1,4 +0,0 @@
-let breadCrumbVariables = ["Home >", "Electronics >", "Game and Console >", 
-	"Nintendo Switch Neon Joy-Con(1 year maxsoft warranty) + screen protector"];
-
-export default breadCrumbVariables;
--- src/utils/commonFunctions.js
+++
@@ -1,5 +0,0 @@
-let templateDecider = ((bool, comp1, comp2) => {
-	return bool ? comp1 : comp2;
-});
-
-export default templateDecider;
